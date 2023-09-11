803feb074b4036fb8cec83596aed7d9b0a184b05
Paul Blessing <80071381+Paul-Blessing777@users.noreply.github.com>
2023-09-11 19:50:55 -0400

    Add files via upload

---
+++ C# School Work/Variables/Variables/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            */
+
+            //string myFirstName;
+            //    myFirstName = "Paul";
+
+            //string myLastName;
+            //myLastName = "Blessing";
+
+            //string myWholeName;
+            //myWholeName = myFirstName + myLastName;
+            //Console.WriteLine(myWholeName);
+
+            var myName1 = "paul";
+            var myName2 = "Blessing";
+            var myName3 = myName1 + myName2;
+            Console.WriteLine(myName3);
+            Console.ReadLine();
+
+            //int x = 7;
+            //string y = "Paul";
+            //string myFirstTry = x + y;
+            //Console.WriteLine(myFirstTry);
+
+            //int x = 7;
+            //string y = "5";
+            //int mySecondTry = x + int.Parse(y);
+            //Console.WriteLine(mySecondTry);
+            //Console.ReadLine();
+        }
+    }
+}
